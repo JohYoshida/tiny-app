f5d5a66320918d3708c7ad898233d25ba7818a9c
Joh Yoshida <johyoshida@gmail.com>
2017-11-08 21:18:48 +0000

    Fixed login/logout for urls/new page

--- express_server.js
+++ express_server.js
@@ -52,7 +52,8 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
