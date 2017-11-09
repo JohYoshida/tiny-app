3ef4b7f948c6b19a168e39f227043e30cba922f3
Joh Yoshida <johyoshida@gmail.com>
2017-11-09 23:05:20 +0000

    Require bcrypt

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt");
 
 // use ejs
 app.set("view engine", "ejs");
